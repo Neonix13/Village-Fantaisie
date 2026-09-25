2d3c9b1fe9080d8a38f003a577f2753c2cc764ae
Neonix13 <130598206+Neonix13@users.noreply.github.com>
2026-09-25 14:09:29 +0200

    Revert "Appli mobile : PWA, version statique et projet Android (Capacitor)"
    This reverts commit d26d8a78034617ac3ca39b99db82f84b4dfc4ed6.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { getPageData } = require('./page');
+const ejsLib = require('ejs');
+const { ROLES, PRIORITY } = require('./roles');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +13,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Le jeu tourne entierement dans le navigateur (etat dans localStorage) : le
 // serveur ne fait que servir la page, le catalogue des roles et les blasons.
-const pageData = getPageData();
+// Les blasons SVG (views/partials/role_badge.ejs) sont pre-rendus une fois ici
+// car le client n'a pas de moteur de templates.
+const roleBadgeTemplatePath = path.join(__dirname, 'views', 'partials', 'role_badge.ejs');
+const roleBadgeTemplate = fs.readFileSync(roleBadgeTemplatePath, 'utf8');
+
+const badges = {};
+Object.values(ROLES).forEach((role) => {
+  badges[role.id] = ejsLib.render(roleBadgeTemplate, { role, size: 'sm' }, { filename: roleBadgeTemplatePath });
+});
+
+const pageData = {
+  rolesJson: JSON.stringify(ROLES),
+  priorityJson: JSON.stringify(PRIORITY),
+  badgesJson: JSON.stringify(badges)
+};
 
 app.get('/', (req, res) => {
   res.render('jeu', pageData);
